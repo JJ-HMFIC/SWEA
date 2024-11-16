b617346021b1a74ca93d9aacc2b14639c0fac7fa
Kyunghwan Jeon <crasheso@gmail.com>
2024-11-16 18:48:47 +0900

    1234 stack

---
+++ 1234.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin =open("../Review/input_1234.txt", "r")
+
+T = 10
+for test_case in range(1,T+1):
+    length, password = input().split()
+
+    stack=[]
+    for c in password:
+        if stack and stack[-1]==c:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    result = ''.join(stack)
+    print(f'#{test_case} {result}')
+
+
